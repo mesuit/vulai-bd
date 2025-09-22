a8aecf03ab26b7a9742fa6288a99b8869d4085f7
sesco001 <marcasmatheka@gmail.com>
2025-09-22 04:31:29 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "supersecret")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///instance/app.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
+    HUMANISE_API_KEY = os.getenv("HUMANISE_API_KEY")
